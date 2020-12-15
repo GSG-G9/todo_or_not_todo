3013a8e92de6d24a169ed5f3325769e3b334f151
Alaa Shurrab <alaa.m.shurrab98@gmail.com>
2020-12-15 15:58:24 +0200

    err handel function

--- src/app.js
+++ src/app.js
@@ -1,15 +1,18 @@
 const express = require('express');
 const { join } = require('path');
 const rauter = require('./router');
+const { e404, serverErr } = require('./controllers/err');
 
 const app = express();
 
 app.use(express.json());
-app.use(express.urlencoded());
+app.use(express.urlencoded({ extended: true }));
 
 app.use(express.static(join(__dirname, '..', 'public')));
-app.use(rauter);
+// app.use(rauter);
 
+app.use(e404);
+app.use(serverErr);
 // err handeler
 
 module.exports = { app };
---
+++ src/controllers/err.js
@@ -0,0 +1,26 @@
+const e404 = (req, res) => {
+  res.send('<h1>404 sorry ,what you\'re looking for isn\'t here </h1>');
+};
+
+const errCatcher = (msg, status) => {
+  const err = new Error();
+  err.msg = msg;
+  err.status = status;
+  return err;
+};
+
+const serverErr = (err, req, res, next) => {
+  res.status(err.status || 500).json({
+    error_det: {
+      errTitle: 'something whent wrong',
+      errStatus: err.status,
+      errMasseage: err.msg || err.masseage,
+    },
+  });
+};
+
+module.exports = {
+  e404,
+  errCatcher,
+  serverErr,
+};
